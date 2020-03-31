87de61688cd45c5f22837c0d27a0173df4813326
unknown <lsayasean@gmail.com>
2020-03-30 22:04:14 -0400

    learning dfs

---
+++ depth_first_search.py
@@ -0,0 +1,18 @@
+# learning how to implement depth first search
+class Node:
+    # python version on constructor
+    def __init__(self, name):
+      self.children = []
+      self.name = name
+
+    # method for adding a name 
+    def addChild(self, name):
+      self.children.append(Node(name))
+      return self
+
+    # depth first search implemented, it calles itself from left to right , each branch at a time 
+    def depthFirstSearch(self, array):
+      array.append(self.name)
+		for child in self.children:
+			child.depthFirstSearch(array)
+		return array
