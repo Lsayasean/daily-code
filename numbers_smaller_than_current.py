495416f62de9c5cd8aecacd0ddcce7647ee8ae08
unknown <lsayasean@gmail.com>
2020-04-18 18:55:11 -0400

    added comments

--- numbers_smaller_than_current.py
+++ numbers_smaller_than_current.py
@@ -1,8 +1,10 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums):
         results = []
+        # sorts the array to compare with current array
         sortedArr = sorted(nums)
         
         for num in nums:
+            # gives index of array which also lets us know how many numbers are currently smaller than current number
             results.append(sortedArr.index(num))
         return results
